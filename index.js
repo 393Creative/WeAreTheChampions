62d4e962a1afa40656c119b365c29c1be3d1fb8f
393Creative <143082004+393Creative@users.noreply.github.com>
2023-11-06 16:36:49 +0800

    Commit 6

--- index.js
+++ index.js
@@ -1,78 +1,80 @@
-import { initializeApp } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js"
-import { getDatabase, ref, push, onValue, } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js"
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js";
+import { getDatabase, ref, push, onValue } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js"
 
 const appSettings = {
-    databaseURL: "https://wearechampsscrim-default-rtdb.firebaseio.com/"
+  databaseURL: "https://wearechampsscrim-default-rtdb.firebaseio.com/"
 }
 
 const app = initializeApp(appSettings)
 const database = getDatabase(app)
+
 const mainElDB = ref(database, "mainEl")
-const fromElDB = ref(database, "From")
-const toElDB = ref(database, "To")
+const fromElDB = ref(database, "fromEl")
+const toElDB = ref(database, "toEl")
 
 const mainEl = document.getElementById("main-input-el")
 const btnEl = document.getElementById("btn-el")
 const listEl = document.getElementById("list-el")
-const fromEl = document.getElementById('from-el')
-const toEl = document.getElementById('to-el')
+const fromEl = document.getElementById("from-el")
+const toEl = document.getElementById("to-el")
 
+btnEl.addEventListener("click", function () {
+  let mainInputValue = mainEl.value
+  let toInputValue = toEl.value
+  let fromInputValue = fromEl.value
 
-btnEl.addEventListener('click', function(){
-    let mainInputValue = mainEl.value
-    let toInputValue = toEl.value
-    let fromInputValue = fromEl.value
-    push(mainElDB, mainInputValue)
-    push(toElDB, toInputValue)
-    push(fromElDB, fromInputValue)
-    fromElClear()
-    toElClear()
-    mainElClear()
+  push(mainElDB, mainInputValue)
+  push(toElDB, toInputValue)
+  push(fromElDB, fromInputValue)
+  formClear()
 })
 
-onValue(mainElDB, function(snapshot) {
+  onValue(mainElDB, function (snapshot) {
+    if (snapshot.exists()) {
+      const mainData = snapshot.val()
+      const mainArray = Object.entries(mainData)
 
-    if (snapshot.exists()){
-        let itemsArray = Object.entries(snapshot.val())
-    
-    listElClear()
-    
-    for (let i = 0; i < itemsArray.length; i++) {
-        let currentItem = itemsArray[i]
+      onValue(fromElDB, function (snapshot) {
+        const toData = snapshot.val()
+        const toArray = Object.entries(toData)
 
-        let currentItemID = currentItem[0]
-        let currentItemValue = currentItem[1]
-        
-        appendToMainDB(currentItem)
-        
-    }}else{
-        let p = document.createElement("p")
-        p.textContent = "No Items Here...... Yet"
-        document.body.appendChild(p)
+        onValue(toElDB, function (snapshot) {
+          const fromData = snapshot.val()
+          const fromArray = Object.entries(fromData)
+
+          listEl.innerHTML = ""
+
+          for (let i = 0; i < mainArray.length; i++) {
+            let currentFromArray = toArray[i][1]
+            let currentMainArray = mainArray[i][1]
+            let currentToArray = fromArray[i][1]
+
+            appendToMainDB(currentFromArray, currentMainArray, currentToArray)
+          }
+        })
+      })
     }
-})
+  })
 
-function listElClear() {
-    listEl.innerHTML = ""
+function formClear() {
+  mainEl.value = ""
+  fromEl.value = ""
+  toEl.value = ""
 }
 
-function mainElClear() {
-    mainEl.value = ''  
-}
-
-function toElClear() {
-    toEl.value = ""
-}
-
-function fromElClear() {
-    fromEl.value = ""
-}
-
-function appendToMainDB(item) {
-    let itemID = item[0]
-    let itemValue = item[1]
-    let newEl = document.createElement("li")
-    newEl.textContent = itemValue
-    listEl.append(newEl)
+function appendToMainDB(from, main, to) {
+    const newLiEl = document.createElement("li")
+    const newH3 = document.createElement("h3")
+    const newH5 = document.createElement("h4")
+  
+    newH3.innerHTML += to
+    newLiEl.appendChild(newH3)
+  
+    newLiEl.innerHTML += main
+  
+    newH5.innerHTML += from
+    newLiEl.appendChild(newH5)
+  
+    listEl.insertBefore(newLiEl, mainEl.firstChild)
 }
 
